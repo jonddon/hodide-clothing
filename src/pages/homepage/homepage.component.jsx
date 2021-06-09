d1460deaecd1e0f007945da49260cc41c6f9151d
John Odey <odeyjohn4u@gmail.com>
2021-06-09 09:23:21 +0100

    Broken homepage into different component and styled the directory menu item

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,63 +1,10 @@
 import React from 'react'
+import Directory from '../../components/directory/directory.component'
 import './homepage.styles.scss'
 const Homepage = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
-                    <div className="content">
-                        <div className="title">
-                            HATS
-                        </div>
-                        <span className="subtitle">
-                            SHOP NOW
-                        </span>
-                    </div>
-                </div>
-                
-                <div className="menu-item">
-                    <div className="content">
-                        <div className="title">
-                            JACKETS
-                        </div>
-                        <span className="subtitle">
-                            SHOP NOW
-                        </span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <div className="title">
-                            SNEAKERS
-                        </div>
-                        <span className="subtitle">
-                            SHOP NOW
-                        </span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <div className="title">
-                            WOMEN
-                        </div>
-                        <span className="subtitle">
-                            SHOP NOW
-                        </span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <div className="title">
-                            MEN
-                        </div>
-                        <span className="subtitle">
-                            SHOP NOW
-                        </span>
-                    </div>
-                </div>
-                
-            </div>
-            
+            <Directory/>      
         </div>
     )
 }
